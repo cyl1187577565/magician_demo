efc90d22a65867266e066a3690f1b22f8f959e3e
yulong.cao <yulong.cao@bianlifeng.com>
2021-03-10 10:08:31 +0800

    增加常量

---
+++ script/src/main/java/common/constants/CylHttpConstants.java
@@ -0,0 +1,32 @@
+package common.constants;
+
+/**
+ * @description:
+ * @author: yulong.cao
+ * @since: 2021-01-11 11:30
+ **/
+public class CylHttpConstants {
+
+    public static final String COOKIE = "gray_hash=0a0384e9-8013-4144-ad3c-3618018763b1; operator_ticket=62ef9644853242088d4de9295f50529c; samesite_operator_ticket=62ef9644853242088d4de9295f50529c; operator_sign=92d03f29931dfdd18a43fd7ee71a8dc1; samesite_operator_sign=92d03f29931dfdd18a43fd7ee71a8dc1; operator_timestamp=1615169083965; samesite_operator_timestamp=1615169083965; user_name=yulong.cao; samesite_user_name=yulong.cao";
+
+
+
+    private static final String PRO_URL = "https://opc.blibee.com";
+    private static final String GRAY_URL = "https://display-gray.corp.bianlifeng.com";
+    private static final String BETA1_URL = "";
+    private static final String BETA2_URL = "";
+    private static final String BETA3_URL = "";
+    private static final String BETA4_URL = "";
+    private static final String DEV1_URL = "";
+    private static final String DEV2_URL = "";
+    private static final String DEV3_URL = "";
+    private static final String DEV4_URL = "";
+
+
+
+
+    public static final String BASE_URL = PRO_URL;
+    public static final String DBA_PROFILE = "prod";
+//    public static final String DBA_PROFILE = "gray";
+
+}
